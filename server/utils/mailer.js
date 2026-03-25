ec3b102ebb663feb48ce43a017154f0d837fa5ae
swaroop-bug <swaroopkapte2324@ternaengg.ac.in>
2026-03-25 12:48:56 +0530

    Fixed mailer

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -6,7 +6,9 @@ const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
